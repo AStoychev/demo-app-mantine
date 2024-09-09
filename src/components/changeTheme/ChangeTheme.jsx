bd1b49cc5a689dc3d46b5ff33836d2e2ca4c6856
Stoychev <stoychev.nas@gmail.com>
2024-09-09 08:23:13 +0300

    Changes related with add dark and light theme toggle in field

--- src/components/changeTheme/ChangeTheme.jsx
+++ src/components/changeTheme/ChangeTheme.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import { useMantineColorScheme, Button, Group } from '@mantine/core';
 
 export const ChangeTheme = () => {
-    const { setColorScheme } = useMantineColorScheme();
+    const { colorScheme, setColorScheme } = useMantineColorScheme();
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', colorScheme);
+    }, [colorScheme]);
 
     return (
         <Group>
