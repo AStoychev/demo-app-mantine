003c8a58bc9d3eb3cc8da1cfaa3511573a20937a
Stoychev <stoychev.nas@gmail.com>
2024-09-13 08:17:57 +0300

    Changes realted with add side bar and table and in App field

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
 import { Headers } from './components/header/Header';
 import { Table } from './components/table/Table';
+// import { OpenCloseSideBarButtons } from './components/Buttons/OpenCloseSideBarButtons';
+import { SideBar } from './components/sideBar/SideBar';
+import { Footer } from './components/footer/Footer';
 import './App.css';
 
 function App() {
@@ -20,7 +23,10 @@ function App() {
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
             <MantineProvider theme={{ colorScheme }}>
                 <Headers />
+                {/* <OpenCloseSideBarButtons/> */}
+                <SideBar />
                 <Table />
+                <Footer />
             </MantineProvider>
         </ColorSchemeProvider>
     );
