a7437118c35086cb04a22f97233bf68ee5a10456
Stoychev <stoychev.nas@gmail.com>
2024-09-16 08:27:12 +0300

    Changes related with format code in App

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     // State to manage light/dark theme
     const localStorageTheme = localStorage.getItem('theme');
     const [colorScheme, setColorScheme] = useState(localStorageTheme);
+
     // Function to toggle between light and dark mode
     const toggleColorScheme = (value) => {
         setColorScheme(value || (colorScheme === 'light' ? 'dark' : 'light'));
