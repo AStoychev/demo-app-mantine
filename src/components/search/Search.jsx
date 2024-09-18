1bac3adfca12d8fd262c98ecea0b127cce958010
Stoychev <stoychev.nas@gmail.com>
2024-09-18 07:46:53 +0300

    Changes related with move openclosesidebarbutton in new directory, new instaler in package json and in header create search field

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Autocomplete } from "@mantine/core";
+
+export const Search = () => {
+    const [value, setValue] = useState('');
+    return <Autocomplete data={[]} value={value} onChange={setValue} />;
+
+}
