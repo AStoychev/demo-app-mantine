447d3d6812056d2b1ace0b291652245884cbc41f
Stoychev <stoychev.nas@gmail.com>
2024-09-19 07:45:26 +0300

    Changes related with Header and SideBar field

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,49 @@
-import { Header, Text, Group, Button, useMantineTheme } from '@mantine/core';
+import { Header, Text, Group, Button, UnstyledButton, useMantineTheme } from '@mantine/core';
+import { DashboardButton } from '../buttons/openCloseSideBarButtons/dashboarButton/DashboardButton';
 import { ChangeTheme } from '../changeTheme/ChangeTheme';
+import { Search } from '../search/Search';
+import { MdDashboard } from "react-icons/md";
+import { AiOutlineStock, AiOutlineNodeIndex } from "react-icons/ai";
+import { FcCurrencyExchange } from "react-icons/fc";
 import styles from "./Header.module.css";
+
+const groupStyles = {
+    padding: '8px',
+    borderRadius: '8px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s, color 0.3s',
+    '&:hover': {
+        backgroundColor: '#4caf50',
+        color: 'white',
+    },
+}
 
 export const Headers = () => {
     const theme = useMantineTheme();
     const color = theme.colors;
-    
+
     return (
         <Header className={styles.header} height={60}>
-            <Group position="apart" style={{ height: '100%' }}>
-                {/* <Text weight={500}>My App</Text> */}
+            <div className={styles.leftSide}>
+                <Text weight={500}>My App</Text>
+                <Search />
+            </div>
+            <div className={styles.rightSide}>
+                <DashboardButton text={"Dashboard"} bg={theme.colorScheme === 'light' ? '#20294C' : '#5AA1C2'} color={'white'}/>
+                <Group sx={groupStyles}>
+                    <AiOutlineStock />
+                    <UnstyledButton>Stocks</UnstyledButton>
+                </Group>
+                <Group sx={groupStyles}>
+                    <FcCurrencyExchange />
+                    <UnstyledButton>Currency</UnstyledButton>
+                </Group>
+                <Group sx={groupStyles}>
+                    <AiOutlineNodeIndex />
+                    <UnstyledButton>Index</UnstyledButton>
+                </Group>
                 <ChangeTheme />
-                {/* <Button variant="outline">Login</Button> */}
-            </Group>
+            </div>
         </Header>
     );
 }
--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,21 +1,63 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Drawer } from '@mantine/core';
-import { OpenCloseSideBarButtons } from '../Buttons/OpenCloseSideBarButtons';
+import { Drawer, Group, Flex, UnstyledButton, Button } from '@mantine/core';
+import { OpenCloseSideBarButtons } from '../buttons/openCloseSideBarButtons/OpenCloseSideBarButtons';
+import { FaHome } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
+import { MdMailOutline } from "react-icons/md";
+import { VscSettingsGear } from "react-icons/vsc";
+import { IoMdLogOut } from "react-icons/io";
+
+const groupStyles = {
+    width: '80%',
+    padding: '10px',
+    borderRadius: '8px',
+    transition: 'background-color 0.3s, color 0.3s',
+    '&:hover': {
+        backgroundColor: '#4caf50',
+        color: 'white',
+    },
+}
 
 export const SideBar = () => {
     const [opened, { open, close, toggle }] = useDisclosure(false);
 
     return (
         <>
-            <Drawer offset={8} radius="md" opened={opened} onClose={close} title="Authentication"
+            <Drawer offset={8} radius="md" size="xs" opened={opened} onClose={close} title="Authentication"
                 closeButtonProps={{
                     size: 'lg',
-                    // color: 'red',
-                    // style: { borderRadius: '50%', borderColor: 'black', color: 'black' }, // Custom CSS styles
                 }}
             >
-                <div>Table</div>
-                <div>Charts</div>
+
+                <Flex
+                    mih={50}
+                    gap="md"
+                    justify="flex-start"
+                    align="flex-start"
+                    direction="column"
+                    wrap="wrap"
+                >
+                    <Group sx={groupStyles}>
+                        <FaHome />
+                        <UnstyledButton>Home</UnstyledButton>
+                    </Group>
+                    <Group sx={groupStyles}>
+                        <FaRegUser />
+                        <UnstyledButton>Profile</UnstyledButton>
+                    </Group>
+                    <Group sx={groupStyles}>
+                        <MdMailOutline />
+                        <UnstyledButton>Message</UnstyledButton>
+                    </Group>
+                    <Group sx={groupStyles}>
+                        <VscSettingsGear />
+                        <UnstyledButton>Settings</UnstyledButton>
+                    </Group>
+                    <Group sx={groupStyles}>
+                        <IoMdLogOut/>
+                        <UnstyledButton>Logout</UnstyledButton>
+                    </Group>
+                </Flex>
             </Drawer>
 
             <OpenCloseSideBarButtons open={open} close={close} toggle={toggle} />
