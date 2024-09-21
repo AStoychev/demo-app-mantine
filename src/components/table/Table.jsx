bbd47b76c1d90743aa50086d9f829cec0266a09d
Stoychev <stoychev.nas@gmail.com>
2024-09-21 22:12:20 +0300

    Cahnges related with create fetch currency data, table design and change theme

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,80 +2,107 @@ import { useMemo } from 'react';
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
 import { useCurrencyData } from '../../hooks/useCurrencyData';
 
-//nested data is ok, see accessorKeys in ColumnDef below
 const data = [
     {
-        name: {
-            firstName: 'Zachary',
-            lastName: 'Davis',
+        price: {
+            open: 'Zachary',
+            close: 'Davis',
+            low: 'Columbus',
+            high: 'Ohio',
+            price: '261 Battle Ford',
         },
-        address: '261 Battle Ford',
-        city: 'Columbus',
-        state: 'Ohio',
+        change: '111111'
     },
-    {
-        name: {
-            firstName: 'Robert',
-            lastName: 'Smith',
-        },
-        address: '566 Brakus Inlet',
-        city: 'Westerville',
-        state: 'West Virginia',
-    },
-    {
-        name: {
-            firstName: 'Kevin',
-            lastName: 'Yan',
-        },
-        address: '7777 Kuhic Knoll',
-        city: 'South Linda',
-        state: 'West Virginia',
-    },
-    {
-        name: {
-            firstName: 'John',
-            lastName: 'Upton',
-        },
-        address: '722 Emie Stream',
-        city: 'Huntington',
-        state: 'Washington',
-    },
-    {
-        name: {
-            firstName: 'Nathan',
-            lastName: 'Harris',
-        },
-        address: '1 Kuhic Knoll',
-        city: 'Ohiowa',
-        state: 'Nebraska',
-    },
+    // {
+    //     name: {
+    //         firstName: 'Robert',
+    //         lastName: 'Smith',
+    //     },
+    //     address: '566 Brakus Inlet',
+    //     city: 'Westerville',
+    //     state: 'West Virginia',
+    // },
+    // {
+    //     name: {
+    //         firstName: 'Kevin',
+    //         lastName: 'Yan',
+    //     },
+    //     address: '7777 Kuhic Knoll',
+    //     city: 'South Linda',
+    //     state: 'West Virginia',
+    // },
+    // {
+    //     name: {
+    //         firstName: 'John',
+    //         lastName: 'Upton',
+    //     },
+    //     address: '722 Emie Stream',
+    //     city: 'Huntington',
+    //     state: 'Washington',
+    // },
+    // {
+    //     name: {
+    //         firstName: 'Nathan',
+    //         lastName: 'Harris',
+    //     },
+    //     address: '1 Kuhic Knoll',
+    //     city: 'Ohiowa',
+    //     state: 'Nebraska',
+    // },
 ];
 
 export const Table = () => {
     const { data: currencyData, error, isLoading, isError } = useCurrencyData('USD', "EUR");
 
+    if (currencyData && currencyData.results && currencyData.results.length > 0) {
+        const result = currencyData?.results[0];
+        // console.log(111111111, result)
+        const openPrice = result.o; // Opening price (start of the day)
+        const closePrice = result.c; // Closing price (end of the day) or Sell price
+        const highPrice = result.h; // Highest price of the day
+        const lowPrice = result.l; // Lowest price of the day
+
+        // Calculate Change
+        const change = closePrice - openPrice;
+
+        console.log('Open Price:', openPrice);
+        console.log('Close Price (Sell):', closePrice);
+        console.log('Change:', change);
+        console.log('USR/EUR Price:', closePrice); // Assuming closing price is the price you need
+        console.log('High Price:', highPrice);
+        console.log('Low Price: ', lowPrice)
+
+    } else {
+        console.log('No results found');
+
+    }
+
     //should be memoized or stable
     const columns = useMemo(
         () => [
             {
-                accessorKey: 'name.firstName', //access nested data with dot notation
-                header: 'First Name',
+                accessorKey: 'price.price', //normal accessorKey
+                header: 'Price',
             },
             {
-                accessorKey: 'name.lastName',
-                header: 'Last Name',
+                accessorKey: 'price.low',
+                header: 'Low Price',
             },
             {
-                accessorKey: 'address', //normal accessorKey
-                header: 'Address',
+                accessorKey: 'price.high',
+                header: 'High Price',
             },
             {
-                accessorKey: 'city',
-                header: 'City',
+                accessorKey: 'change',
+                header: 'Changes',
             },
             {
-                accessorKey: 'state',
-                header: 'State',
+                accessorKey: 'price.close',
+                header: 'Close Price',
+            },
+            {
+                accessorKey: 'price.open', //access nested data with dot notation
+                header: 'Open Price',
             },
         ],
         [],
--- src/functions/fetchCurrencyData.js
+++ src/functions/fetchCurrencyData.js
@@ -3,6 +3,38 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const fetchCurrencyData = async (fromCurrency, toCurrency) => {
-    const { data } = await axios.get(`https://api.polygon.io/v2/aggs/ticker/C:${fromCurrency}${toCurrency}/prev?apiKey=${API_KEY}`);
-    return data
+    const currentDate = new Date();
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setMonth(currentDate.getMonth() - 1);
+
+    try {
+
+        const { data } = await axios.get(`https://api.polygon.io/v2/aggs/ticker/C:${fromCurrency}${toCurrency}/prev?apiKey=${API_KEY}`);
+
+        // if (data && data.results && data.results.length > 0) {
+        //     const result = data.results[0];
+
+        //     const openPrice = result.o; // Opening price (start of the day)
+        //     const closePrice = result.c; // Closing price (end of the day) or Sell price
+        //     const highPrice = result.h; // Highest price of the day
+        //     const lowPrice = result.l; // Lowest price of the day
+
+        //     // Calculate Change
+        //     const change = closePrice - openPrice;
+
+        //     console.log('Open Price:', openPrice);
+        //     console.log('Close Price (Sell):', closePrice);
+        //     console.log('Change:', change);
+        //     console.log('USR/EUR Price:', closePrice); // Assuming closing price is the price you need
+        //     console.log('High Price:', highPrice);
+        //     console.log('Low Price: ', lowPrice)
+        // } else {
+        //     console.log('No results found');
+        // }
+
+        return data
+    }
+    catch (error) {
+        console.log('ERROR', error)
+    }
 }
