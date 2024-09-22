957e37a199970022aa193ca943ef105c8573a383
Stoychev <stoychev.nas@gmail.com>
2024-09-22 21:32:24 +0300

    Changes realted with add OpenCloseSideBarButtons styles and DashboardButton

---
+++ src/components/buttons/openCloseSideBarButtons/dashboarButton/DashboardButton.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import { MdDashboard } from "react-icons/md";
+import styles from "./DashboardButton.module.css";
+
+export const DashboardButton = ({ text, bg, color, hoverBg, }) => {
+    const [isOnHover, setIsOnHover] = useState(false);
+
+    const buttonStyle = {
+        background: isOnHover ? hoverBg : bg,
+        color: color
+    }
+
+    return (
+        <div
+            className={styles.wrapper}
+            style={buttonStyle}
+            onMouseEnter={() => setIsOnHover(true)}
+            onMouseLeave={() => setIsOnHover(false)}
+        >
+            <MdDashboard />
+            {text}
+        </div>
+    )
+}
