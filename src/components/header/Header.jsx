9deb1d11f68a1ae35ca833ceefb1a9b183d8e3ce
Stoychev <stoychev.nas@gmail.com>
2024-09-25 08:23:23 +0300

    Changes related with new functionality in App and Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,9 @@
-import { Header, Text, Group, Button, UnstyledButton, useMantineTheme } from '@mantine/core';
+import { Header, Text, Group, UnstyledButton, useMantineTheme } from '@mantine/core';
 import { DashboardButton } from '../buttons/openCloseSideBarButtons/dashboarButton/DashboardButton';
 import { ChangeTheme } from '../changeTheme/ChangeTheme';
-// import { Search } from '../search/Search';
-import { MdDashboard } from "react-icons/md";
 import { AiOutlineStock, AiOutlineNodeIndex } from "react-icons/ai";
 import { FcCurrencyExchange } from "react-icons/fc";
+import { fetchHistoricalData } from '../../functions/fetchHistoricalData';
 import styles from "./Header.module.css";
 
 const groupStyles = {
@@ -21,10 +20,11 @@ const groupStyles = {
 export const Headers = () => {
     const theme = useMantineTheme();
 
+    fetchHistoricalData();
+
     return (
         <Header className={styles.header} height={60}>
             <div className={styles.leftSide}>
-                {/* <Search /> */}
             </div>
             <div className={styles.rightSide}>
                 <DashboardButton text={"Dashboard"} bg={theme.colorScheme === 'light' ? '#20294C' : '#5AA1C2'} color={'white'}/>
