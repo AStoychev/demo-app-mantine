b9da98a58a20baf340738487764ce6b537482a4a
Stoychev <stoychev.nas@gmail.com>
2024-10-16 07:49:44 +0300

    Changes related with add useCurrencyData hook about get by  minutes data

--- src/hooks/useCurrencyData.js
+++ src/hooks/useCurrencyData.js
@@ -6,7 +6,6 @@ export const useCurrencyData = (fromCurrency, toCurrency) => {
     return useQuery({
         queryKey: ["currencyData", fromCurrency, toCurrency],
         queryFn: fetchAllCurrencyData,
-        // queryFn: () => fetchCurrencyData(fromCurrency, toCurrency),
         refetchOnWindowFocus: false,
         staleTime: 1000 * 60 * 5, // Data is fresh for 5 minutes
         cacheTime: 1000 * 60 * 10, // Cache data for 10 minutes
