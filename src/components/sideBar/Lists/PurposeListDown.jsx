34105d84c3b526e0d232f7f8ccab6a1b30f3972f
Stoychev <stoychev.nas@gmail.com>
2024-10-25 07:47:02 +0300

    Changes related with add purpose lust down and purpose list top like a one by one file in the field

---
+++ src/components/sideBar/Lists/PurposeListDown.jsx
@@ -0,0 +1,17 @@
+import { Text } from "@mantine/core"
+
+export const PurposeListDown = () => {
+    return (
+        <>
+            <Text>
+                This app is for demonstration purposes only and is designed to highlight my expertise in building scalable,
+                interactive applications. I hope this demo provides a clear example of how these technologies work together to
+                create a rich, real-time data experience.
+            </Text>
+
+            <Text>
+                Feel free to explore the features, and don’t hesitate to reach out with any questions or feedback!
+            </Text>
+        </>
+    )
+}
---
+++ src/components/sideBar/Lists/PurposeListTop.jsx
@@ -0,0 +1,11 @@
+import { Text } from "@mantine/core"
+
+export const PurposeListTop = () => {
+    return (
+        <Text>
+            This app has been built to showcase my full-stack development skills using cutting-edge technologies.
+            The purpose of this demo is to display Stock and Forex data, along with interactive charts
+            and tables for enhanced user interaction and data visualization.
+        </Text>
+    )
+}
