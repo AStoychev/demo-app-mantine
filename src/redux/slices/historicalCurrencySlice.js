eee5e53b2b87050b151c07af642460bd10f7cf9b
Stoychev <stoychev.nas@gmail.com>
2024-10-27 15:57:46 +0200

    Changes related with add historical field

---
+++ src/redux/slices/historicalCurrencySlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const historicalCurrencySlice = createSlice({
+    name: 'historicalCurrency',
+    initialState: {
+        data: {},
+        isLoading: false,
+        error: null
+    },
+    reducers: {
+        setHistoricalCurrencyData: (state, action) => {
+            state.data = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.isLoading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        }
+    }
+});
+
+export const { setHistoricalCurrencyData, setLoading, setError } = historicalCurrencySlice.actions;
+export default historicalCurrencySlice.reducer;
