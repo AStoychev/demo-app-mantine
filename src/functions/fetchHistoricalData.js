f40f5e30f3ec53629a4fecd044988b629707afcf
Stoychev <stoychev.nas@gmail.com>
2024-10-28 07:44:15 +0200

    Changes related with add function for fetch historical data from poligon api field

--- src/functions/fetchHistoricalData.js
+++ src/functions/fetchHistoricalData.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { getDateBackOneMount } from "./getDateBackOneMount";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-const REQUEST_DELAY = 10000; // 10 seconds delay for retry
+const API_KEY = import.meta.env.VITE_API_HISTORICAL_KEY;
+// const REQUEST_DELAY = 10000; // 10 seconds delay for retry
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const fetchHistoricalData = async ({ currencyOne, currencyTwo }) => {
     const { startDate, endDate } = getDateBackOneMount();
@@ -17,24 +17,25 @@ export const fetchHistoricalData = async ({ currencyOne, currencyTwo }) => {
             const closePrices = response.data.results.map((item) => item.c);
 
             return { t: timestamps, c: closePrices, pairs: [currencyOne, currencyTwo] };
-        } else {
-            throw new Error("No data in response");
         }
+        // else {
+        //     throw new Error("No data in response");
+        // }
     } catch (error) {
         // Suppress sensitive info
-        if (error.response) {
-            if (error.response.status === 429) {
-                console.warn("Rate limit exceeded, waiting for 10 seconds before retrying...");
-                await delay(REQUEST_DELAY); // Wait before retrying
-                return fetchHistoricalData({ currencyOne, currencyTwo }); // Retry the request
-            } else {
-                // Log a custom message without exposing API details
-                console.error("Error fetching historical data: Rate limit exceeded or server error.");
-            }
-        } else {
-            // Log a generic error if no response
-            console.error("Error fetching historical data: No response from server.");
-        }
+        // if (error.response) {
+        //     if (error.response.status === 429) {
+        //         console.warn("Rate limit exceeded, waiting for 10 seconds before retrying...");
+        //         // await delay(REQUEST_DELAY); // Wait before retrying
+        //         return fetchHistoricalData({ currencyOne, currencyTwo }); // Retry the request
+        //     } else {
+        //         // Log a custom message without exposing API details
+        //         console.error("Error fetching historical data: Rate limit exceeded or server error.");
+        //     }
+        // } else {
+        //     // Log a generic error if no response
+        //     console.error("Error fetching historical data: No response from server.");
+        // }
         throw error; // Rethrow to handle it elsewhere if needed
     }
 }
