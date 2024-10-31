bec7ed7c689a17c9c89b9d64543f4d1dcb4ec0da
Stoychev <stoychev.nas@gmail.com>
2024-10-31 07:45:37 +0200

    Chamges related with add use currency data functionality field

--- src/hooks/useCurrencyData.js
+++ src/hooks/useCurrencyData.js
@@ -1,13 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
 import { fetchAllCurrencyData } from "../functions/fetchCurrencyData";
+import { setCurrencyData, setLoading, setError } from "../redux/slices/currencySlice";
 
 export const useCurrencyData = (fromCurrency, toCurrency) => {
-    return useQuery({
+    const dispatch = useDispatch();
+
+    const { data, error, isLoading, isError} = useQuery({
         queryKey: ["currencyData", fromCurrency, toCurrency],
         queryFn: fetchAllCurrencyData,
         refetchOnWindowFocus: false,
         staleTime: 1000 * 60 * 5, // Data is fresh for 5 minutes
         cacheTime: 1000 * 60 * 10, // Cache data for 10 minutes
         retry: 2,
-    })
+    });
+    dispatch(setCurrencyData(data))
+    return { data, error, isLoading, isError }
 }
