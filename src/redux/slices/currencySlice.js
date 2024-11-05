ea5c000fbd382f5ed6cec9ca6d1fa8bc02aaed66
Stoychev <stoychev.nas@gmail.com>
2024-11-05 07:45:26 +0200

    Changes realted with add currency Slice functionality filed

--- src/redux/slices/currencySlice.js
+++ src/redux/slices/currencySlice.js
@@ -24,28 +24,3 @@ const currencySlice = createSlice({
 
 export const { setCurrencyData, setLoading, setError } = currencySlice.actions;
 export default currencySlice.reducer;
-
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const currencySlice = createSlice({
-//     name: 'currency',
-//     initialState: {
-//         data: {},
-//         isLoading: false,
-//         error: null
-//     },
-//     reducers: {
-//         setCurrencyData: (state, action) => {
-//             state.data = action.payload;
-//         },
-//         setLoading: (state, action) => {
-//             state.isLoading = action.payload;
-//         },
-//         setError: (state, action) => {
-//             state.error = action.payload;
-//         }
-//     }
-// });
-
-// export const { setCurrencyData, setLoading, setError } = currencySlice.actions;
-// export default currencySlice.reducer;
