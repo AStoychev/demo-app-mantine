3dcb84c88c59e26abf1fa53d13ff5601457c69d3
Stoychev <stoychev.nas@gmail.com>
2024-11-09 16:31:00 +0200

    Changes related with add Chart and App stie and styles in field

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,33 @@
+import { Text, Flex } from "@mantine/core";
+import { AreaChart } from "../areaChart/AreaChart";
+import { useHistoricalData } from "../../hooks/useHistoricalData";
+import styles from "./Chart.module.css";
+
+export const Chart = () => {
+    const currencyPairs = [
+        { from: "EUR", to: "USD"},
+        { from: "GBP", to: "USD"},
+        { from: "USD", to: "CHF"},
+    ];
+
+    return (
+        <Flex className={styles.wrapper} align="center">
+            <Flex className={styles.dataWrapper}>
+                {currencyPairs.map(({from, to}) => {
+                    const { data, isLoading, error } = useHistoricalData(from, to);
+
+                    if (isLoading) {
+                        return <Text key={`${from}-${to}`}>Loading {from}/{to}...</Text>;
+                    }
+                    if (error) {
+                        return <Text key={`${from}-${to}`}>Error loading {from}/{to}</Text>;
+                    }
+
+                    return (
+                        <AreaChart key={`${from}/${to}`} instruments={`${from}/${to}`}/>
+                    );
+                })}
+            </Flex>
+        </Flex>
+    )
+}
