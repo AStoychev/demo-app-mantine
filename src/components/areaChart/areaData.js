a9326cfbed4b0b2e69d32bc35e2d2605c6a0a672
Stoychev <stoychev.nas@gmail.com>
2024-12-03 07:30:31 +0200

    Changes related with add area data in field

--- src/components/areaChart/areaData.js
+++ src/components/areaChart/areaData.js
@@ -1,27 +1,8 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler,
-} from "chart.js";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler );
 
 const data = [
     { t: 1724630400000, c: 1.11645 },
